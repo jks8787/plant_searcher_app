9634836b74037ae9211310264bbb4d3d15c2726d
Janice Smith <janice.k.n.smith@gmail.com>
2017-01-31 23:35:28 -0500

    update to seeds file, to create plant records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_data = File.read('lib/assets/plants_list.csv')
+csv = CSV.parse(csv_data, :headers => true)
+csv.each do |row|
+  hash = row.to_hash
+  Plant.create!(name: hash["Scientific Name"], symbol: hash["Accepted Symbol"])
+end
